6cf9203a2489fbd67d1b6c3ebb4a731607a3d341
AlexisChartier <alexischartier30130pse@gmail.com>
2023-06-05 13:36:24 +0200

    deploiement

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Définir le chemin vers le répertoire de build de l'application Angular
+const distPath = path.join(__dirname, 'dist');
+
+// Définir le dossier de fichiers statiques
+app.use(express.static(distPath));
+
+// Toutes les routes mènent à l'index.html pour la gestion du routage côté client
+app.get('', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+// Démarrer le serveur sur le port 8080
+app.listen(8080, () => {
+  console.log('Serveur Express en cours d\'exécution sur le port 8080');
+});
